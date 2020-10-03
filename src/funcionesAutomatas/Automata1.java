3157cf701d91946f9d0d0013c263a8d576af30ca
Alexander Chali <alexmapascc@gamil.com>
2020-10-03 08:12:11 -0600

    La cague

--- src/funcionesAutomatas/Automata1.java
+++ src/funcionesAutomatas/Automata1.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package funcionesAutomatas;
 
-/**
- *
- * @author Ronaldo
- */
+
 public class Automata1 {
 
     public static String ejecutar(String cadena, int linea) {
